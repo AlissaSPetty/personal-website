652d893085c232ab98c93c3c572f7f40eb7d0014
Alissa Taylor <alissashaytaylor@gmail.com>
2016-05-01 19:55:18 -0400

    add: work on header

--- themes-dev/alissa/homepage.php
+++ themes-dev/alissa/homepage.php
@@ -3,11 +3,21 @@
 ?>
 <head>
   <title>Alissa Taylor | Atlanta, GA</title>
-  <link rel="stylesheet" href="/css/custom.min.css" />
-  <link rel="javascript" href="/js/script.js" />
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/custom.min.css' ?>" />
+  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+  <script type="text/javascript" href="<?php echo get_stylesheet_directory_uri().'/js/script.js' ?>"></script>
 </head>
 
 
 <div class="homepage">
-<div class="header">Hello this is a test</div>
+  <div class="header" style="background:url(<?php echo get_stylesheet_directory_uri().'/images/header-bg.jpg' ?>); background-size: cover; height: 50vh; width: auto">
+    <h1>Web Developer</h1>
+    <div class="underline"></div>
+    <p>Hi my name is Alissa Taylor, I am a 25 year old Web Developer based in Atlanta, GA. I currently work at a video game company, but I am avaliable for freelance work. </p>
+    <div class="social-media">
+      <a href=""><i class="ion-social-linkedin"></i></a>
+      <a href=""><i class="ion-social-github"></i></a>
+      <a href=""><i class="ion-social-facebook"></i></a>
+    </div>
+  </div>
 </div>
