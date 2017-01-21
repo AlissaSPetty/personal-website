9bf73ba8df81103d29e2b5cb5557a10495ff59e9
Alissa Taylor <alissataylor@Alissas-MacBook-Pro.local>
2017-01-21 17:31:11 -0500

    big site redo

--- themes-dev/alissa/homepage.php
+++ themes-dev/alissa/homepage.php
@@ -1,6 +1,4 @@
-<? php
-//Template Name: Homepage
-?>
+<? php //Template Name: Alissa Homepage 2 ?>
 <head>
   <title>Alissa Taylor | Atlanta, GA</title>
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/custom.min.css' ?>" />
@@ -21,83 +19,113 @@
       <p>Alissa Taylor</p>
     </div>
     <ul class="nav small-12 columns">
-      <li class="small-2 columns"><a href="#">About</a></li>
-      <li class="small-2 columns"><a href="#">Skills</a></li>
-      <li class="small-2 columns"><a href="#">My Work</a></li>
-      <li class="small-2 columns"><a href="#">Contact</a></li>
+      <li class="small-2 columns"><a href="#about">About</a></li>
+      <li class="small-2 columns"><a href="#skills">Skills</a></li>
+      <li class="small-2 columns"><a href="#work">My Work</a></li>
+      <li class="small-2 columns"><a href="#contact">Contact</a></li>
     </ul>
   </div>
+  <a id="about"></a>
   <div class="header row fullWidth" style="background:url(<?php echo get_stylesheet_directory_uri().'/images/header-bg.jpg' ?>); background-size: cover; width: auto">
     <h1>Web Developer</h1>
     <div class="underline"></div>
-    <p class="small-12 columns">Hi my name is Alissa Taylor, I am a 26 year old Web Developer based in Atlanta, GA. I am looking for a full-time position within an amazing new company. </p>
+    <p class="small-12 columns">Hi my name is Alissa Taylor, I am a 26 year old Web Developer based in Atlanta, GA. I am actively taking new clients for freelance projects. </p>
     <div class="social-media">
       <a href=""><i class="ion-social-linkedin linkedin"></i></a>
       <a href=""><i class="ion-social-github github"></i></a>
       <a href=""><i class="ion-social-facebook facebook"></i></a>
     </div>
   </div>
+  <a id="skills"></a>
+  <a id="work"></a>
   <div class="projects-container row fullWidth">
-    <h1 class="projects">Projects I have kicked ass on</h1>
-    <div class="p1 small-12 medium-4 columns">
-      <a href="">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Tribes_Universe.png" alt="Tribes Universe" />
+    <h1 class="projects">Projects</h1>
+    <div class="p1 small-12 medium-3 columns">
+      <a href="http://www.smiterivals.com">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/www_smiterivals_com.jpg" alt="SMITE Rivals" />
         <div class="about">
-          <p class="title">Tribes Universe</p>
-          <p class="date">Date</p>
-          <p class="hover">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut placerat vulputate arcu. Curabitur non ullamcorper nisl, et condimentum ligula. </p>
+          <p class="title">SMITE Rivals</p>
+          <p class="date">January 2017</p>
+          <p class="hover">http://www.smiterivals.com</p>
         </div>
       </a>
     </div>
-    <div class="p2 small-12 medium-4 columns">
-      <a href="">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Midseason_Update.png" alt="SMITE Midseason Update" />
+    <div class="p2 small-12 medium-3 columns">
+      <a href="http://www.smitegame.com/celtic/">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Path_of_the_Phantom_Queen___SMITE.jpg" alt="SMITE Celtic Event" />
         <div class="about">
-          <p class="title">SMITE Midseason Update</p>
-          <p class="date">Date</p>
-          <p class="hover">. Donec id ultricies massa, sit amet euismod nunc. Nunc fermentum facilisis pretium. In facilisis elementum pretium.</p>
+          <p class="title">SMITE Celtic Event</p>
+          <p class="date">December 2016</p>
+          <p class="hover">http://www.smitegame.com/celtic/</p>
         </div>
       </a>
     </div>
-    <div class="p3 small-12 medium-4 columns">
-      <a href="">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Sol_Reveal.png" alt="Sol God Reveal" />
+    <div class="p3 small-12 medium-3 columns">
+      <a href="http://www.smitegame.com/fafnirs-winter-wonderland/">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Fafnir’s_Winter_Wonderland___SMITE.jpg" alt="Fafnir's Winter Wonderland" />
         <div class="about">
-          <p class="title">Sol God Reveal</p>
-          <p class="date">Date</p>
-          <p class="hover">Nam commodo sapien id massa ultricies convallis. Donec non magna ex. Nunc in mauris ut lacus ullamcorper placerat ut et eros.</p>
+          <p class="title">Fafnir's Winter Wonderland</p>
+          <p class="date">December 2016</p>
+          <p class="hover">http://www.smitegame.com/fafnirs-winter-wonderland/</p>
         </div>
       </a>
     </div>
-    <div class="p4 small-12 medium-4 columns">
-      <a href="">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Xing-Tian_Reveal.png" alt="Xing Tian God Reveal" />
+    <div class="p4 small-12 medium-3 columns">
+      <a href="http://www.hirezexpo.com/">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Hi-Rez_Expo___2017.jpg" alt="2017 Hi-Rez Expo" />
         <div class="about">
-          <p class="title">Xing Tian God Reveal</p>
-          <p class="date">Date</p>
-          <p class="hover">Vivamus eget malesuada ligula, eu scelerisque libero. Donec efficitur, nunc non sodales facilisis, orci magna tempor ex, eu aliquet metus ipsum vitae leo.</p>
+          <p class="title">Hi-Rez Expo</p>
+          <p class="date">November 2016</p>
+          <p class="hover">http://www.hirezexpo.com</p>
         </div>
       </a>
     </div>
-    <div class="p5 small-12 medium-4 columns">
-      <a href="">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Season_3.png" alt="SMITE Season 3 Update" />
+    <div class="p5 small-12 medium-3 columns">
+      <a href="https://www.smitegame.com/midseason-2016/">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Midseason_Update___SMITE.jpg" alt="SMITE Season 3 Update" />
         <div class="about">
           <p class="title">SMITE Season 3 Update</p>
-          <p class="date">Date</p>
-          <p class="hover">Aliquam convallis metus enim, nec egestas mi commodo sit amet. Cras vitae eros eu nunc dapibus hendrerit.</p>
+          <p class="date">June 2016</p>
+          <p class="hover">https://www.smitegame.com/midseason-2016/</p>
         </div>
       </a>
     </div>
-    <div class="p6 small-12 medium-4 columns">
-      <a href="">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Paladins.png" alt="Paladins" />
+    <div class="p6 small-12 medium-3 columns">
+      <a href="http://www.paladins.com">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Paladins.jpg" alt="Paladins" />
         <div class="about">
           <p class="title">Paladins</p>
-          <p class="date">Date</p>
-          <p class="hover">Quisque tincidunt facilisis libero vitae aliquet. Nunc aliquet leo vitae scelerisque varius. Quisque venenatis suscipit odio, at consequat nunc condimentum et.</p>
+          <p class="date">January 2016</p>
+          <p class="hover">http://www.paladins.com</p>
         </div>
       </a>
+    </div>
+    <div class="p7 small-12 medium-3 columns">
+      <a href="http://www.smitegame.com/promo/god-reveal/sol/">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/SMITE_-_Sol__Goddess_of_the_Sun_🔊.jpg" alt="Sol God Reveal" />
+        <div class="about">
+          <p class="title">Sol God Reveal</p>
+          <p class="date">August 2015</p>
+          <p class="hover">http://www.smitegame.com/promo/god-reveal/sol/</p>
+        </div>
+      </a>
+    </div>
+    <div class="p8 small-12 medium-3 columns">
+      <a href="http://www.tribesuniverse.com/">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Tribes_Universe.jpg" alt="Tribes Universe" />
+        <div class="about">
+          <p class="title">Tribes Universe</p>
+          <p class="date">June 2015</p>
+          <p class="hover">http://www.tribesuniverse.com/</p>
+        </div>
+      </a>
+    </div>
+  </div>
+  <a id="contact"></a>
+  <h5>I would love to hear from you! Fill out the form below for more information about freelance projects, or just to say hello!</h5>
+  <div class="contact">
+    <div class="form">
+      <?php echo do_shortcode('[contact-form-7 id="60" title="Contact form"]') ?>
     </div>
   </div>
 </div>
