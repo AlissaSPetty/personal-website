699b5da371d3440d1cef2db21520d6c74ec0a90e
alissastaylor <alissataylor@Alissas-MacBook-Pro.local>
2017-08-28 10:46:05 -0400

    add: basic wedding website structure

---
+++ themes-dev/alissa/wedding.php
@@ -0,0 +1,57 @@
+<?php
+/**
+* Template Name: A+B = Wedding
+*
+*/
+?> 
+<div class="wedding">
+  <div class="header">
+    <h1>Brandon and Alissa Are Getting Married!</h1>
+    <p>May 5, 2018</p>
+  </div>
+  <div class="about">
+    <h2>About the Couple</h2>
+    <p>Brandon and Alissa have been dating for almost 4 years! They met around Christmas-time in 2013, and have been inseperable since! </p>
+  </div>
+  <div class="wedding-party">
+    <div class="bridesmaids">
+    <h3>Bride's Side</h3>
+      <p>Blair Taylor <span>Sister of the Bride</span><i> Maid of Honor</i></p>
+      <p>Lindsay Greer <span>Friend of the Bride</span><i> Marton of Honor</i></p>
+      <p>Andee Cantatore <span>Friend of the Bride</span><i> Bridesmaid</i></p>
+      <p>Ansli Pemberton <span>Friend of the Bride</span><i> Bridesmaid</i></p>
+      <p>Erin Martin <span>Friend of the Bride</span><i> Bridesmaid</i></p>
+    </div>
+    <div class="groomsmen">
+    <h3>Groom's Side</h3>
+       <p>TBD</p>
+    </div>
+    <div class="pups">
+      <h3>Special Pups of Honor</h3>
+      <p>Charlie</p> <img src="" alt="">
+      <p>Mason</p> <img src="" alt="">
+    </div>
+  </div>
+  <div class="venue">
+    <a href=""><h3>Fendley Farmstead</h3></a>
+    <h4>Canton, GA</h4>
+    <img src="" alt="">
+    <p>Fendley Farmstead is home to a beautiful barn with indoor seating and air conditioning. As soon as the couple saw the location, they knew that's where they would say "I do".</p>
+  </div>
+  <div class="accomodations">
+    <h3>Accomodations</h3>
+    <p>TBD</p>
+  </div>
+  <div class="rsvp">
+    <h2>Want to come? We would love to see you there!</h2>
+  </div>
+  <div class="registry">
+    <h2>Registry</h2>
+    <p>The couple are registered at the following places:</p>
+    <ul>
+      <a href=""><li>Amazon</li></a>
+      <a href=""><li>Honemoon Fund</li></a>
+      <a href=""><li>Best Buy</li></a>
+    </ul>
+  </div>
+</div>
